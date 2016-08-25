103c291a9e6a6cc5b050fb4c61f170fc64049beb
liujohn008@gmail.com <0okm9ijn>
2016-08-24 22:32:30 -0400

    fix

--- ForTT.py
+++ ForTT.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 
-data_file1 = pd.read_excel(r"/Users/Emily/Desktop/Python/TestData/file1.xlsx",parse_cols = r"A,B,C")
+data_file1 = pd.read_excel(r"/Users/Emily/Desktop/Python/TestData/file1.xlsx",parse_cols = "A,B,C")
+data_file1 = pd.read_excel(r"/Users/Emily/Desktop/Python/TestData/file1.xlsx")
 data_filew = pd.read_excel(r"/Users/Emily/Desktop/Python/TestData/filew.xlsx")
 
 data_filew['a1'] = data_filew['b1']
 
-result = pd.merge(data_file1, data_filew, how='outer', on=['a1']).fillna(0).sort(columns = 'a1', ascending = True)
+result = pd.merge(data_file1, data_filew, how='outer', on=['a1']).fillna(0).sort_values(by = 'a1', ascending = True)
 
 result['Minus'] = result['b13'] - result['a13']
 
